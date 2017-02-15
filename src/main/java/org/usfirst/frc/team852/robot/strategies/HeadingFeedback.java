86759e68d727d3dbd10f0a650dbf44f3d859bafe
Paul Ambrose <pambrose@mac.com>
2017-02-15 09:06:34 -0800

    Update comment

--- src/main/java/org/usfirst/frc/team852/robot/strategies/HeadingFeedback.java
+++ src/main/java/org/usfirst/frc/team852/robot/strategies/HeadingFeedback.java
@@ -13,13 +13,12 @@ public class HeadingFeedback {
     }
 
     public double getError(final double recentVal) {
-        // Assume that we are no more than 45 degrees off line
+        // Assume that we are no more than 90 degrees off line
         if (this.initialHeading <= 90 + 45)
             return recentVal - (recentVal > (180 + 45) ? (this.initialHeading + 360) : this.initialHeading);
         else if (this.initialHeading >= 270 - 45)
             return (recentVal < (180 - 45) ? (recentVal + 360) : recentVal) - this.initialHeading;
         else
             return recentVal - this.initialHeading;
-
     }
 }
